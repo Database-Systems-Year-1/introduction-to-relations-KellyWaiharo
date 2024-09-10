f9fa975da0de9102a0836e1572127ebdd7d16ba0
KellyWaiharo <167400893+KellyWaiharo@users.noreply.github.com>
2024-09-10 12:10:40 +0300

    Add files via upload

---
+++ students.sql
@@ -0,0 +1,9 @@
+CREATE TABLE STUDENS(
+	id INT,
+    name VARCHAR(30),
+	course VARCHAR(30),
+	year INT,
+	primary key(id));
+
+	insert into STUDENS(id,name,course,year)
+	VALUES(1025,'Jane','BBIT',2024)
