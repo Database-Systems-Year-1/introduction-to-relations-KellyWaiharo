fcbe53e014f9bf373653add0f3b77f68110c00ae
KellyWaiharo <167400893+KellyWaiharo@users.noreply.github.com>
2024-09-10 23:02:14 +0300

    Add files via upload

---
+++ empolyee.sql
@@ -0,0 +1,9 @@
+CREATE TABLE employee(
+	id INT,
+	name VARCHAR(20),
+	salary INT,
+	department VARCHAR(50),
+	primary key (id));
+
+	insert into employee(id,name,salary,department)
+	VALUES(1234,'John',10000,'Finance')
---
+++ product.sql
@@ -0,0 +1,9 @@
+CREATE TABLE product(
+	number INT,
+	name VARCHAR(30),
+	price INT,
+	description VARCHAR(100),
+	primary key (number));
+
+	insert into product(number,name,price,description)
+	VALUES(12,'DESK',2000,'brown')
